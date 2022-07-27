1a722edefa05c6eb71fa427c1c0bb87583f2d253
jessicarushford <jessicalynnrushford@gmail.com>
2022-07-27 13:26:38 -0400

    added repos and controllers

---
+++ src/test/java/guru/springframework/spring5webfluxrest/controllers/CategoryControllerTest.java
@@ -0,0 +1,21 @@
+package guru.springframework.spring5webfluxrest.controllers;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CategoryControllerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void list() {
+    }
+
+    @Test
+    public void getById() {
+    }
+}
